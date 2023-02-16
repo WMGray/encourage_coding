fd5958cb708573280ff5ab3b160c04f1ae4b8438
WMGray <1466787434@qq.com>
2023-02-16 21:44:20 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # encourage_coding
+
+> playsound库需要修改：
+>
+> - 将源码中`command = ' '.join(command).encode('utf-16')`变为`command = ' '.join(command)`即可
--- sounding.py
+++ sounding.py
@@ -4,24 +4,24 @@ from playsound import playsound
 from path import Path
 import random
 import time
-import threading
-
 
 last_press_time = 0  # 记录上次按下的时间
 cooldown_end_time = 0  # 记录冷却结束的时间
-play_voice = False
 
 sound_dir = "sounds_file"
-roles = os.listdir(sound_dir)
+roles = [Path(sound_dir)/x for x in os.listdir(sound_dir)]
+print(roles)
+type_1, type_2 = os.listdir(roles[0])
+audio_1 = [Path(roles[0])/Path(type_1)/x
+           for x in os.listdir(Path(roles[0])/type_1)]
+audio_2 = [Path(roles[0])/Path(type_2)/x
+           for x in os.listdir(Path(roles[0])/type_2)]
 
-type_1, type_2 = roles[0], roles[1]
-audio_1 = [Path(sounds_files)/Path(type_1)/x for x in os.listdir(Path(sound_dir)/type_1)]
-audio_2 = [Path(sounds_files)/Path(type_2)/x for x in os.listdir(Path(sound_dir)/type_2)]
 
-
-def play_audio(audio_filename):
+def play_audio(audio_filename, sleep_time: int):
     # 播放音频的函数
-    print(audio_filename)
+    playsound(audio_filename)
+    time.sleep(sleep_time)
 
     
 def on_enter_press():
@@ -31,22 +31,17 @@ def on_enter_press():
     
     if current_time < cooldown_end_time:  # 判断是否在冷却中
         return
-    print("时间间隔：", current_time - last_press_time)
-    print("上次摁下时间", time.asctime( time.localtime(current_time) ))
+
     if current_time - last_press_time < 10:
         # 时间间隔小于 10s
-        threading.Timer(0, play_audio, args=[1]).start()
-        time.sleep(2)
-    elif 10 <= current_time - last_press_time :
+        play_audio(random.choice(audio_1), 2)
+    elif 10 <= current_time - last_press_time:
         # 时间间隔在 10s- 
-        threading.Timer(0, play_audio, args=[2]).start()
-        time.sleep(5)
+        play_audio(random.choice(audio_2), 3)
 
     # 更新上次按下的时间
     last_press_time = time.time()
-    print("这次摁下时间", time.asctime( time.localtime(last_press_time) ))
-    print()
-    
-"""keyboard.add_hotkey("enter", on_enter_press)
-keyboard.wait()  # 一直等待键盘事件的发生"""
-print(audio_1)
+
+
+keyboard.add_hotkey("enter", on_enter_press)
+keyboard.wait()  # 一直等待键盘事件的发生
